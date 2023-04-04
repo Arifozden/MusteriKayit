8243039935eeb9b45ffe8e5252e702b0082f1bf4
Arifozden <111346692+Arifozden@users.noreply.github.com>
2023-04-04 13:32:05 +0200

    Initial commit

---
+++ src/test/java/oslomet/musteri/MusteriApplicationTests.java
@@ -0,0 +1,13 @@
+package oslomet.musteri;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MusteriApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
